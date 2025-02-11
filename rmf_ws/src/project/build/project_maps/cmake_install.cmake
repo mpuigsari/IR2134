55fc93a587052858c7cad51a8cbd4942d8522508
mpuigsari <al415556@uji.es>
2025-02-11 19:03:32 +0100

    Desesperación
    Compila y crea el mundo pero al ejecutar launch no hace nada. Cambiada versión de gazebo y consultado github rmf_demos

--- rmf_ws/src/project/build/project_maps/cmake_install.cmake
+++ rmf_ws/src/project/build/project_maps/cmake_install.cmake
@@ -43,6 +43,10 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/project_maps" TYPE DIRECTORY FILES "/home/max/Documents/GitHub/IR2134/rmf_ws/src/project/project_maps/maps/")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/max/Documents/GitHub/IR2134/rmf_ws/src/project/build/project_maps/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/project_maps")
 endif()
 
@@ -99,6 +103,10 @@ endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/project_maps" TYPE FILE FILES "/home/max/Documents/GitHub/IR2134/rmf_ws/src/project/project_maps/package.xml")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/project_maps/maps" TYPE DIRECTORY FILES "/home/max/Documents/GitHub/IR2134/rmf_ws/src/project/build/project_maps/maps/test1")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
--- rmf_ws/src/project/build/project_simulation/colcon_command_prefix_build.sh
+++ rmf_ws/src/project/build/project_simulation/colcon_command_prefix_build.sh
@@ -1,5 +1,1 @@
 # generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/max/Documents/GitHub/IR2134/rmf_ws/src/project/install/project_assets/share/project_assets/package.sh"
-. "/home/max/Documents/GitHub/IR2134/rmf_ws/src/project/install/project_fleet_adapter/share/project_fleet_adapter/package.sh"
-. "/home/max/Documents/GitHub/IR2134/rmf_ws/src/project/install/project_maps/share/project_maps/package.sh"
-. "/home/max/Documents/GitHub/IR2134/rmf_ws/src/project/install/project_config/share/project_config/package.sh"
